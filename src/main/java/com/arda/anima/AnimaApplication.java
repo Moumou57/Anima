239d232f11f5abb6706369108ff570f95a804706
Jance Kayre <maxime_auvray@hotmail.com>
2017-09-15 15:58:16 +0200

    Initial commit

---
+++ src/main/java/com/arda/anima/AnimaApplication.java
@@ -0,0 +1,12 @@
+package com.arda.anima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arda/anima/AnimaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arda.anima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
